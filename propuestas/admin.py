fb8b7947ec88c01e6cce161f689dd0ab96a1e76e
elvus <e.s95@hotmail.com>
2019-05-18 11:43:34 -0400

    proposal model

--- propuestas/admin.py
+++ propuestas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Proposal
+
 # Register your models here.
+
+admin.site.register(Proposal)
+#Registramos el modelo propuestas
--- propuestas/models.py
+++ propuestas/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class Proposal(models.Model):
+	"""docstring for Proposal"""
+	name = models.TextField()
+	email = models.EmailField()
+	category = models.TextField()
+	title = models.TextField()
+	content = models.TextField()
+	approved = models.BooleanField()
+
+	def __str__(self):
+		return self.name
+		
