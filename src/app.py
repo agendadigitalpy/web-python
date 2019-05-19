7ee8a5d36232fef09b02c685ab2d3c4b5a1c1c44
carloscarvallo <carloscarvallo@protonmail.com>
2019-05-19 15:57:18 -0400

    fix bug #19 y formateo de pylint

--- src/app.py
+++ src/app.py
@@ -13,105 +13,123 @@ app = Flask(__name__,  static_url_path='')
 app.config["MONGO_URI"] = "mongodb://localhost:27017/agendadigitalpy"
 mongo = PyMongo(app)
 
+
 @app.route('/assets/<path:path>')
 def send_assets(path):
-	return send_from_directory('assets', path)
+    return send_from_directory('assets', path)
+
 
 @app.route('/')
 def index():
-	return render_template('principal.html')
-	
+    return render_template('principal.html')
+
 
 @app.route("/propuestas/<_id>")
 def show_proposal(_id):
     proposal = mongo.db.proposals.find_one({'_id': ObjectId(_id)})
     return render_template("propuesta.html", proposal=proposal)
 
-@app.route('/propuestas', methods = ['GET'])
+
+@app.route('/propuestas', methods=['GET'])
 def getProps():
-	proposals = mongo.db.proposals.find().sort('_id', DESCENDING)
-	return render_template("propuestas.html", proposals=proposals)
+    msg = {}
+    proposals = mongo.db.proposals.find().sort('_id', DESCENDING)
+    return render_template("propuestas.html", proposals=proposals, message=msg)
 
 # TODO: validar e-mail
-@app.route('/propuestas', methods = ['POST'])
+@app.route('/propuestas', methods=['POST'])
 def createProposal():
-        name = remove_html(request.form["name"])
-        email = remove_html(request.form["email"])
-        category = request.form["category"]
-        try:
-                category = int(category)
-                if category > 4:
-                        category = 0
-        except ValueError:
-                category = 0
-        title = remove_html(request.form["title"])
-        content = remove_html(request.form["content"])
-        p = {
-                'name': name,
-                'email': email,
-                'category': category,
-                'title': title,
-                'content': content,
-                'approved': True
-        }
+    name = remove_html(request.form["name"])
+    email = remove_html(request.form["email"])
+    category = request.form["category"]
+    try:
+        category = int(category)
+        if category > 4:
+            category = 0
+    except ValueError:
+        category = 0
+    title = remove_html(request.form["title"])
+    content = remove_html(request.form["content"])
+    p = {
+        'name': name,
+        'email': email,
+        'category': category,
+        'title': title,
+        'content': content,
+        'approved': True
+    }
 
-        msg = {}
-        if len(parseaddr(p["email"][1])) == 0 :
-                msg["text"] =  "Email invalido."
-                msg["success"] = False
-        else:
-                msg["success"] = True
-                mongo.db.proposals.insert(p)
+    msg = {}
+    if len(parseaddr(p["email"][1])) == 0:
+        msg["text"] = "Email invalido."
+        msg["success"] = False
+    else:
+        msg["success"] = True
+        mongo.db.proposals.insert(p)
 
-        proposals = mongo.db.proposals.find().sort('_id', DESCENDING)
-        return render_template("propuestas.html", proposals=proposals, message=msg)
+    proposals = mongo.db.proposals.find().sort('_id', DESCENDING)
+    return render_template("propuestas.html", proposals=proposals, message=msg)
 
 
 def remove_html(s):
-        soup = BeautifulSoup(s)
-        return soup.get_text()
+    soup = BeautifulSoup(s)
+    return soup.get_text()
+
 
 @app.route('/conectividad')
 def getConectividad():
-	return render_template("conectividad.html")
+    return render_template("conectividad.html")
+
 
 @app.route('/documentacion')
 def getDocumentacion():
-	return render_template("documentacion.html")
+    return render_template("documentacion.html")
+
 
 @app.route('/economia-digital')
 def getEconomia_Digital():
-	return render_template("economia-digital.html")
+    return render_template("economia-digital.html")
+
 
 @app.route('/fortalecimiento-institucional')
 def getFortalecimieto_digital():
-	return render_template("fortalecimiento-institucional.html")
+    return render_template("fortalecimiento-institucional.html")
+
 
 @app.route('/gobierno-digital')
 def getGobierno_digital():
-	return render_template("gobierno-digital.html")
+    return render_template("gobierno-digital.html")
+
 
 @app.route('/seguimiento')
 def getSeguimiento():
-	return render_template("seguimiento.html")
+    return render_template("seguimiento.html")
+
 
 @app.route('/solicitudes')
 def getSolicitudes():
-	return render_template("solicitudes.html")
+    return render_template("solicitudes.html")
 
-cats = ['General', 'Gobierno Digital', 'Economía Digital', 'Conectividad', 'Fortalecimiento Institucional']
+
+cats = ['General', 'Gobierno Digital', 'Economía Digital',
+        'Conectividad', 'Fortalecimiento Institucional']
+
+
 @app.template_filter('category_name')
 def category_name(n):
-        return cats[n]
+    return cats[n]
+
 
 @app.template_filter('proposal_date')
 def proposal_date(i):
-        return i.generation_time.strftime('%d/%m/%y')
+    return i.generation_time.strftime('%d/%m/%y')
+
 
 @app.template_filter('content_filter')
 def content_filter(s):
-        s = s.replace("\n", "<br/>")
-        return s
+    s = s.replace("\n", "<br/>")
+    return s
+
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
